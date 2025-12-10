a23ae5ed70f11c71dd92e4a8f9bcaf96956fcac3
Cʜɪᴋᴜ 🐾 <167273277+MaybeChiku@users.noreply.github.com>
2025-12-10 15:01:19 +0530

    remove unuseful

--- src/__init__.py
+++ src/__init__.py
@@ -31,8 +31,6 @@ class Bot(pyrogram.Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            parse_mode=pyrogram.enums.ParseMode.HTML,
-            max_concurrent_transmissions=7,
         )
         self.owner = config.OWNER_ID
         self.logger = config.LOGGER_ID
